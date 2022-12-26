7d3bf0c22ee967283550822c71557881e0bceda4
Edson Pareja <parejaayma@gmail.com>
2022-12-26 20:25:31 -0300

    subiendo a git

---
+++ js/products.js
@@ -0,0 +1,35 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Campera",
+    precio: 4000,
+    img:
+      "../img/campera.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Buzo",
+    precio: 3500,
+    img:
+      "../img/buzo.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Palazo",
+    precio: 3000,
+    img:
+      "../img/palazo.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "jogging",
+    precio: 2500,
+    img:
+      "../img/jogging.jpg",
+    cantidad: 1,
+  },
+  
+];
